bbc315bec4501a2ada2265531df4097c335da9bf
Lenar Khafizov <lenarkhaf@gmail.com>
2025-02-24 20:09:39 +0300

    Выполнено задание 1

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 13676;
+        int bonus = 20;
+        int bonusSum = ticketPrice/bonus;
+        System.out.println("Вам начислено " + bonusSum + " бонус(-а,-ов)");
+    }
+}
