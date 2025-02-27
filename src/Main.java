4c71ec857a701b44ac642ff5591a1403851c4f9b
Lenar Khafizov <lenarkhaf@gmail.com>
2025-02-27 19:51:08 +0300

    Выполнено форматирование кода в задании 1

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int ticketPrice = 13676;
         int bonus = 20;
-        int bonusSum = ticketPrice/bonus;
+        int bonusSum = ticketPrice / bonus;
         System.out.println("Вам начислено " + bonusSum + " бонус(-а,-ов)");
     }
 }
